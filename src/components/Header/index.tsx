5adb6d41dc4e4e320de7b535b179e723ab090b73
João Hugo Leda <joaohugoleda2@gmail.com>
2022-03-09 21:18:03 -0300

    header maint

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 import styles from './header.module.scss'
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <h1>
-        <img src="/images/Logo.svg"/>
-      </h1>
+    <header className={styles.header}>    
+      <Link href="/">
+        <img src="/images/Logo.svg" alt="logo"/>
+      </Link>  
     </header>
   )
 }
